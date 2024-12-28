43f77b954c6349fa0f9c00ddf4ba1583d64e9196
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2024-12-28 18:02:39 +0900

    photos.jsonの場所を移動させた

--- ci/run-pre-push.sh
+++ ci/run-pre-push.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 python3 ci/get_photos.py
 
-if git diff --quiet photos.json; then
+if git diff --quiet src/data/photos.json; then
   echo "No changes to photos.json, skipping commit."
 else
-  git add photos.json
+  git add src/data/photos.json
   git commit -m "Update photos.json"
   git push origin head
 fi
